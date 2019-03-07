c11255a17563d56cadd620a6eff1a020e43b532d
Bruno Philipe <git@bruno.ph>
2019-03-07 21:20:39 +0100

    Fix uploading image attachments

--- Sources/MastodonKit/Foundation/Data.swift
+++ Sources/MastodonKit/Foundation/Data.swift
@@ -14,12 +14,12 @@ extension Data {
 
         self.init()
 
-        append("--MastodonKitBoundary\r\n")
+        append("--\(Payload.formBoundary)\r\n")
         append("Content-Disposition: form-data; name=\"file\"; filename=\"\(mediaAttachment.fileName)\"\r\n")
         append("Content-Type: \(mediaAttachment.mimeType)\r\n\r\n")
         append(mediaData)
         append("\r\n")
-        append("--MastodonKitBoundary--\r\n")
+        append("--\(Payload.formBoundary)--\r\n")
     }
 
     mutating func append(_ string: String?) {
