579608bf8e0a252ace3538df3dd078541da76b96
Bruno Philipe <git@bruno.ph>
2019-05-09 23:36:17 +0200

    Add v2 search request

--- Sources/MastodonKit/Requests/Search.swift
+++ Sources/MastodonKit/Requests/Search.swift
@@ -10,13 +10,32 @@ import Foundation
 
 /// `Search` requests.
 public enum Search {
-    /// Searches for content.
+    /// Searches for content. Only available from Mastodon version 2.4.1.
+    ///
+    /// - Parameters:
+    ///   - query: The search query.
+    ///   - limit: The count limit of results to return.
+    ///   - resolve: Whether to resolve non-local accounts.
+    /// - Returns: Request for `Results`.
+    public static func search(query: String, limit: Int? = nil, resolve: Bool? = nil) -> Request<Results> {
+        let toLimitBounds = between(1, and: 80, default: 40)
+        let parameters = [
+            Parameter(name: "q", value: query),
+            Parameter(name: "resolve", value: resolve.flatMap(trueOrNil)),
+            Parameter(name: "limit", value: limit.map(toLimitBounds).flatMap(toOptionalString))
+        ]
+
+        let method = HTTPMethod.get(.parameters(parameters))
+        return Request<Results>(path: "/api/v2/search", method: method)
+    }
+
+    /// Searches for content using the v1 API.
     ///
     /// - Parameters:
     ///   - query: The search query.
     ///   - resolve: Whether to resolve non-local accounts.
     /// - Returns: Request for `Results`.
-    public static func search(query: String, resolve: Bool? = nil) -> Request<Results> {
+    public static func fallbackSearch(query: String, resolve: Bool? = nil) -> Request<Results> {
         let parameters = [
             Parameter(name: "q", value: query),
             Parameter(name: "resolve", value: resolve.flatMap(trueOrNil))
