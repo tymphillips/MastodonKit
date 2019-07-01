22f9ac0d50820fafd6bed45d8ed9f210f1c9d7f1
Bruno Philipe <git@bruno.ph>
2019-07-02 00:53:30 +0200

    Remove duplicate Tag entity

--- Sources/MastodonKit/Models/Results.swift
+++ Sources/MastodonKit/Models/Results.swift
@@ -14,5 +14,5 @@ public class Results: Codable {
     /// An array of matchhed statuses.
     public let statuses: [Status]
     /// An array of matched hashtags, as strings.
-    public let hashtags: [Hashtag]
+    public let hashtags: [Tag]
 }
